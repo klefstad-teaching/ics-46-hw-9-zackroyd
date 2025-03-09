ee4b491521b614bbbb85650d36a799859752e5fe
zackroyd <zackroyd@uci.edu>
2025-03-09 18:07:31 +0000

    Added debugging statements

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -42,6 +42,8 @@ vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& prev
 vector<int> extract_shortest_path(const vector<int>& distances, const vector<int>& previous, int destination) {
     stack<int> reversed_result;
     vector<int> result;
+    for (int i : previous) std::cout << i << ' ';
+    std::cout << std::endl;
     for (int curr = destination; curr != -1; curr = previous[curr]) {
         reversed_result.push(curr);
         if (reversed_result.size() > distances.size() + 10) break;
