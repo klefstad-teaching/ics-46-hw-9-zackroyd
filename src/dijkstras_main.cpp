2028798b2dc3aa20bb7acc2879fba555f1ac6fe1
zackroyd <zackroyd@uci.edu>
2025-03-09 16:55:13 +0000

    Added cooldown check to extract_shortest_path

--- src/dijkstras_main.cpp
+++ src/dijkstras_main.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 
 int main() {
-    std::string filename = "src/large.txt";
+    std::string filename = "src/largest.txt";
     Graph G;
     file_to_graph(filename, G);
     vector<int> previous;
